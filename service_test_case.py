2443461701aac685a564c00b1e64e29f43803aef
Chris Brackert <cbrackert@gmail.com>
2016-08-31 12:31:17 -0700

    Include publishers and subscribers in base test

--- service_test_case.py
+++ service_test_case.py
@@ -1,10 +1,13 @@
 import requests
+from threading import Event
 from nio import Block
 from nio.block.context import BlockContext
+from nio.modules.communication.publisher import Publisher
+from nio.modules.communication.subscriber import Subscriber
 from nio.testing.test_case import NIOTestCase
 from nio.router.context import RouterContext
 from niocore.core.loader.discover import Discover
-from tests.router import ServiceTestRouter
+from service_tests.router import ServiceTestRouter
 
 
 class NioServiceTestCase(NIOTestCase):
@@ -15,6 +18,14 @@ class NioServiceTestCase(NIOTestCase):
         super().__init__(methodName)
         self._blocks = {}
         self._router = ServiceTestRouter()
+        # Subscribe to publishers in the service
+        self._subscribers = {}
+        # Capture published signals for assertions
+        self.published_signals = []
+        # Set an event when those publishers publish signals
+        self._publisher_event = Event()
+        # Allow tests to publish signals to any subscriber
+        self._publishers = {}
 
     def get_test_modules(self):
         return {'settings', 'scheduler', 'persistence', 'communication'}
@@ -61,12 +72,55 @@ class NioServiceTestCase(NIOTestCase):
         # Start blocks
         for block in self._blocks:
             self._blocks[block].start()
+        # Init publishers and subscribers for tests
+        self._setup_pubsub()
 
     def tearDown(self):
         super().tearDown()
+        # Tear down publishers and subscribers for tests
+        self._teardown_pubsub()
         # Stop blocks
         for block in self._blocks:
             self._blocks[block].stop()
+
+    def publisher_topics(self):
+        """Topics this service publishes to"""
+        return []
+
+    def subscriber_topics(self):
+        """Topics this service subscribes to"""
+        return []
+
+    def _setup_pubsub(self):
+        # Supscribe to published signals
+        for publisher_topic in self.publisher_topics():
+            self._subscribers[str(publisher_topic)] = \
+                    Subscriber(self._published_signals, **publisher_topic)
+        for subscriber in self._subscribers:
+            self._subscribers[subscriber].open()
+        # Allow tests to publish to subscribers in service
+        for subscriber_topic in self.subscriber_topics():
+            self._publishers[str(subscriber_topic)] = \
+                    Publisher(**subscriber_topic)
+        for publisher in self._publishers:
+            self._publishers[publisher].open()
+
+    def _teardown_pubsub(self):
+        self.published_signals = []
+        for subscriber in self._subscribers:
+            self._subscribers[subscriber].close()
+        for publisher in self._publishers:
+            self._publishers[publisher].close()
+        self._publisher_event = Event()
+
+    def publish_signals(self, topic, signals):
+        self._publishers[topic].send(signals)
+
+    def _published_signals(self, signals):
+        # Save published signals for assertions
+        self.published_signals.extend(signals)
+        self._publisher_event.set()
+        self._publisher_event.clear()
 
     def override_block_configs(self):
         """Optionally override block config for the tests"""
@@ -77,3 +131,20 @@ class NioServiceTestCase(NIOTestCase):
             block_config["name"], block_config)
         new_block_config["name"] = block_config["name"]
         return new_block_config
+
+    def wait_for_published_signals(self, count=0, timeout=1):
+        """Wait for the specified number of signals to be published
+
+        If no count is specified, then wait for the next signals to be
+        published. If no signals are published before the timeout, then fail
+        the test.
+
+        Either returns after successfully waiting or fails an assertion
+        """
+        if not count:
+            # Wait for next publisher event
+            return self._publisher_event.wait(timeout)
+        else:
+            # Wait for specified number of signals
+            while(count > len(self.published_signals)):
+                return self._publisher_event.wait(timeout)
--- template_test_file.py
+++ template_test_file.py
@@ -2,78 +2,24 @@ from threading import Event
 from nio.modules.communication.subscriber import Subscriber
 from nio.modules.communication.publisher import Publisher
 from nio.signal.base import Signal
-from tests.service_test_case import NioServiceTestCase
+from service_tests.service_test_case import NioServiceTestCase
 
 
 class TestExample(NioServiceTestCase):
 
     service_name = "Example"
 
-    def __init__(self, methodName='runTests'):
-        super().__init__(methodName)
-        # Subscribe to a publisher
-        self._subsciber = None
-        # Set an event when that publisher sends signals
-        self._publisher_event = Event()
-        self.published_signals = []
-        # Allow tests to publish signals to any subscriber
-        self._publishers = {}
-        # Time to wait for published signals
-        self._publisher_wait_timeout = 1
-
-    def setUp(self):
-        super().setUp()
-        # Supscribe to published signals
-        topic = {"subscriber": ["one"]}
-        self._subscriber = Subscriber(self._published_signals,
-                                      matching_provider="",
-                                      **topic)
-        self._subscriber.open()
-        # Allow tests to publish to subscribers in service
-        self._publishers["publisher_one"] = Publisher(publisher=["one"])
-        self._publishers["publisher_two"] = Publisher(publisher=["two"])
-        for publisher in self._publishers:
-            self._publishers[publisher].open()
-
-    def tearDown(self):
-        super().tearDown()
-        self.published_signals = []
-        self._subscriber.close()
-        for publisher in self._publishers:
-            self._publishers[publisher].close()
-        self._publisher_event = Event()
-
-    def _publish_signals(self, topic, signals):
-        self._publishers[topic].send(signals)
-
-    def _published_signals(self, signals):
-        # Save published signals for assertions
-        self.published_signals.extend(signals)
-        self._publisher_event.set()
-        self._publisher_event.clear()
-
-    def wait_for_published_signals(self, count=0):
-        """Wait for the specified number of signals to be published
-
-        If no count is specified, then wait for the next signals to be
-        published. If no signals are published before the timeout, then fail
-        the test.
-
-        Either returns after successfully waiting or fails an assertion
-        """
-        if not count:
-            # Wait for next publisher event
-            self.assertTrue(
-                self._publisher_event.wait(self._publisher_wait_timeout))
-        else:
-            # Wait for specified number of signals
-            while(count > len(self.published_signals)):
-                self.assertTrue(
-                    self._publisher_event.wait(self._publisher_wait_timeout))
+    def subscriber_topics(self):
+        """Topics this service subscribes to"""
+        return [
+            {"key": ["value"]}
+        ]
 
     def test_service(self):
-        self._publish_signals("publisher_one", [Signal({"input": "signal"})])
+        subscriber_topic = str(self.subscriber_topics()[0])
+        self.publish_signals(subscriber_topic, [Signal({"input": "signal"})])
         self.wait_for_published_signals(1)
         self.assertEqual(len(self.published_signals), 1)
-        self.assertTrue({"output": "signal"},
-                        self.published_signals[-1].to_dict())
+        self.assertDictEqual(self.published_signals[-1].to_dict(), {
+            "output": "signal"
+        })
